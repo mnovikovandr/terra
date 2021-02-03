ec29639fb35f2052df923869cdb8f81bf9d7ae64
Misha <misha.novikau@gmail.com>
2021-02-03 12:09:00 +0300

    change main.tf add group

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "azurerm" {
     features {}
      msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
