36725a9689a8707e5fe74a025bce1982a6e67eb9
Misha <misha.novikau@gmail.com>
2021-02-03 14:41:11 +0300

    7

--- variables.tf
+++ variables.tf
@@ -1,26 +1,3 @@
-variable "location" {}
-
 variable "name" {
 	default = "misha"
-}
-
-variable "prefix" {
-  type    = string
-  default = "my"
-}
-
-variable "tags" {
-  type = map
-
-  default = {
-    Environment = "Terraform GS"
-    Dept        = "Engineering"
-  }
-}
-
-variable "sku" {
-  default = {
-    westus2 = "16.04-LTS"
-    eastus  = "18.04-LTS"
-  }
 }
