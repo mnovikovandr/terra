f386abd2540385a6f333ed3d26d0f5b9e11d4bbc
Misha <misha.novikau@gmail.com>
2021-02-03 10:22:54 +0000

    2

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "azurerm" {    
-    version = "2.45.1"
-    features {}
-     msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"  
+ }
+  }
+}
+
+provider "azurerm" {
+  features {}
+ msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
 resource "azurerm_resource_group" "rg" {
