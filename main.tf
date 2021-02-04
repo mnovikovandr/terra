29e1291aaf3e7f0f6606d9075762c7b124b0944c
Misha <misha.novikau@gmail.com>
2021-02-04 16:22:02 +0300

    43

--- main.tf
+++ main.tf
@@ -12,15 +12,15 @@ provider "azurerm" {
  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
-resource "azurerm_resource_group" "misha" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "storagegroup" {
+  name     = "storagegroup"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storageaccount" {
   name                     = "examplestoraccount"
-  resource_group_name      = azurerm_resource_group.misha.name
-  location                 = azurerm_resource_group.misha.location
+  resource_group_name      = azurerm_resource_group.storagegroup.name
+  location                 = azurerm_resource_group.storagegroup.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -29,8 +29,8 @@ resource "azurerm_storage_account" "example" {
   }
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "vhds"
-  storage_account_name  = azurerm_storage_account.example.name
+resource "azurerm_storage_container" "stcontainer" {
+  name                  = "con"
+  storage_account_name  = azurerm_storage_account.storageaccount.name
   container_access_type = "private"
 }
