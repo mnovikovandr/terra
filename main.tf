6a6137b632cfc016053ba2adc87ada40bdafd605
Misha <misha.novikau@gmail.com>
2021-02-04 16:08:42 +0300

    40

--- main.tf
+++ main.tf
@@ -12,21 +12,21 @@ provider "azurerm" {
  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
-resource "azurerm_resource_group" "mygroup" {
+resource "azurerm_resource_group" "example" {
   name     = "mygroup"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "store" {
+resource "azurerm_storage_account" "example" {
   name = "mystore"
-  resource_group_name = "${azure_resource_group.mygroup.name}"
-  location = "${azure_resource_group.mygroup.location}"
+  resource_group_name = "${azure_resource_group.example.name}"
+  location = "${azure_resource_group.example.location}"
   account_tier = "Standart"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "mycontainer" {
+resource "azurerm_storage_container" "example" {
   name = "terraformtest"
-  storage_account_name = "${azurerm_storage_account.store.name}"
+  storage_account_name = "${azurerm_storage_account.example.name}"
   container_access_type = "private"
 }
