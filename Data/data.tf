b687e184494754a6f464685e3e9a7257548cb5b3
Misha <misha.novikau@gmail.com>
2021-02-05 10:57:19 +0300

    50

--- Data/data.tf
+++ Data/data.tf
@@ -1,19 +1,10 @@
-data "azurerm_resource_group" "example" {
-    name = "arg"
-}
-
-resource "azurerm_managed_disk" "example" {
-    name = "managed_disk_name"
-    location = "West Europe"
-    resource_group_name = "arg"
-    storage_account_type = "Standart_LRS"
-    create_option = "Empty"
-    disc_size_gb = "1"
+data "azurerm_resource_group" "storagegroup" {
+    name = "storagegroup"
 }
 
 terraform {
     backend "azurerm" {
-        storage_account_name = "tfsorrrandomwname"
+        storage_account_name = "examples1t1o1raccount"
         container_name = "con"
         key = "Data.terraform.tfstage"
     }
