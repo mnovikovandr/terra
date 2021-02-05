b7a4cb8f8ef50c6fb553c1ddc8682f3701a91042
Misha <misha.novikau@gmail.com>
2021-02-05 10:38:38 +0300

    add data

--- Data/data.tf
+++ Data/data.tf
@@ -4,5 +4,17 @@ data "azurerm_resource_group" "example" {
 
 resource "azurerm_managed_disk" "example" {
     name = "managed_disk_name"
-    location
+    location = "West Europe"
+    resource_group_name = "arg"
+    storage_account_type = "Standart_LRS"
+    create_option = "Empty"
+    disc_size_gb = "1"
+}
+
+terraform {
+    backend "azurerm" {
+        storage_account_name = "tfsorrrandomwname"
+        container_name = "con"
+        key = "Data.terraform.tfstage"
+    }
 }
