14547a0032f1a6df8755ffebb42b39d138340544
Misha <misha.novikau@gmail.com>
2021-02-05 14:55:56 +0300

    1

--- remouteStorage/main.tf
+++ remouteStorage/main.tf
@@ -12,18 +12,6 @@ provider "azurerm" {
  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
-resource "azurerm_storage_account" "misha87account" {
-  name                     = "misha87account"
-  resource_group_name      = "cloud-shell-storage-westeurope"
-  location                 = "West Europe"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    storage = "test"
-  }
-}
-
 resource "azurerm_storage_container" "csb100320010eec47c0" {
   name                  = "storagecontainer"
   storage_account_name  = "cloud-shell-storage-westeurope"
