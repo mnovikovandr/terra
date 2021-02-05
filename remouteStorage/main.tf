005a5de18731ac4507095a8bf40e785505d16bba
Misha <misha.novikau@gmail.com>
2021-02-05 15:23:35 +0300

    4

--- remouteStorage/main.tf
+++ remouteStorage/main.tf
@@ -12,10 +12,25 @@ provider "azurerm" {
  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
-resource "azurerm_storage_container" "mishastorcontainer" {
-  name                  = "storagecontainer"
-  storage_account_name  = "cloud-shell-storage-westeurope"
-  container_access_type = "private"
+resource "azurerm_resource_group" "ymngroup" {
+  name     = "ymngroup"
+  location = "West Europe"
 }
 
+resource "azurerm_storage_account" "ymnstoreaccount" {
+  name                     = "ymnstoreaccount"
+  resource_group_name      = azurerm_resource_group.ymngroup.name
+  location                 = azurerm_resource_group.ymngroup.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "stcontainer" {
+  name                  = "storecontainer"
+  storage_account_name  = azurerm_storage_account.ymnstoreaccount.name
+  container_access_type = "private"
+}
