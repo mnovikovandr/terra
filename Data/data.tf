b654bd534cf3c90a80561f655ab4487d0676d888
Misha <misha.novikau@gmail.com>
2021-02-05 11:19:19 +0300

    55

--- Data/data.tf
+++ Data/data.tf
@@ -3,6 +3,11 @@ provider "azurerm" {
  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "mygroup"
+  location = "West Europe"
+}
+
 data "azurerm_resource_group" "storagegroup" {
     name = "storagegroup"
 }
