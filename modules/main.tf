5d1af72594496fb70e1799a10f1015a7eb018d93
Misha <misha.novikau@gmail.com>
2021-02-08 10:06:07 +0300

    modules

--- modules/main.tf
+++ modules/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "example" {
     name = "virtualNetwork"
     location = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
-    adress_space = ["10.0.0.0/16"]
+    address_space = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "example" {
