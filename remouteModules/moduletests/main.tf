a93ddabf297854c3310daee6cdc85ce4ccb765e6
Misha <misha.novikau@gmail.com>
2021-02-08 16:34:55 +0300

    modules

--- remouteModules/moduletests/main.tf
+++ remouteModules/moduletests/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "example" {
-    name = resource_group
+    name = var.resource_group
     location = location
 }
 
 resource "azurerm_virtual_network" "example" {
     name = vnet
-    location = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
+    location = var.azurerm_resource_group.example.location
+    resource_group_name = var.azurerm_resource_group.example.name
     address_space = [address_space]
 }
 
 resource "azurerm_subnet" "subnet" {
     name = subnet_name
-    virtual_network_name = azurerm_virtual_network.example.name
-    resource_group_name = azurerm_resource_group.example.name
+    virtual_network_name = var.azurerm_virtual_network.example.name
+    resource_group_name = var.azurerm_resource_group.example.name
     address_prefixes = subnet_prefix
 }
