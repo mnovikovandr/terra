3785f02a2d433b4eb5c90bbde2b981912e0ac558
Misha <misha.novikau@gmail.com>
2021-02-08 10:16:31 +0300

    modules

--- modules/main.tf
+++ modules/main.tf
@@ -1,18 +1,11 @@
 resource "azurerm_resource_group" "example" {
-    name = "exampleGroup"
-    location = "West Europe"
+    name = "${var.resource_group}"
+    location = "${var.location}"
 }
 
 resource "azurerm_virtual_network" "example" {
-    name = "virtualNetwork"
-    location = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
-    address_space = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-    name = "testsubnet"
-    resource_group_name = azurerm_resource_group.example.name
-    virtual_network_name = azurerm_virtual_network.example.name
-    address_prefix = "10.0.1.0/24"
+    name = "${var.vnet}"
+    location = "${azurerm_resource_group.example.location}"
+    resource_group_name = "${azurerm_resource_group.example.name}"
+    address_space = ["var.address_space"]
 }
--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,19 @@ provider "azurerm" {
    features {}
    msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
+
+variable "resource_group" {
+    default = "testmodulegroup"
+}
+
+variable "location" {
+    default = "West Europe"
+}
+
+variable "vnet" {
+    default = "modulevnet"
+}
+
+variable "address_space" {
+    default = "10.0.0.0/16"
+}
