f1e27ed2dc9d853a9ef99f319c32d0b7db2e132d
Misha <misha.novikau@gmail.com>
2021-02-08 10:04:15 +0300

    modules

--- modules/main.tf
+++ modules/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
     location = "West Europe"
 }
 
-resource "azurerm_virtual _network" "example" {
+resource "azurerm_virtual_network" "example" {
     name = "virtualNetwork"
     location = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
