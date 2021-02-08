131ef3c04645f68687edb963084dd7e7dcfacbe7
Misha <misha.novikau@gmail.com>
2021-02-08 13:10:06 +0300

    modules

--- testModules/main.tf
+++ testModules/main.tf
@@ -1,3 +1,8 @@
 module "moduletests" {
     source = "github.com/mnovikovandr/terra/remouteModules/moduletests"
 }
+
+resource "azurerm_resource_group" "res" {
+  name     = "groupAfterModule"
+  location = "West Europe"
+}
