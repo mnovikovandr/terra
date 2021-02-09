4c968896159815a73aa4ddcf1cec0d3a37871e58
Misha <misha.novikau@gmail.com>
2021-02-09 09:58:04 +0300

    modules

--- remouteModules/moduletests/main.tf
+++ remouteModules/moduletests/main.tf
@@ -10,14 +10,14 @@ resource "azurerm_resource_group" "novikovTerraformGroup"  {
 
 resource "azurerm_virtual_network" "novikovTerraformGroupNetwork" {
     name = "novikovVirtualNet"
-    location = azurerm_resource_group.novikovterraformgroup.location
-    resource_group_name = azurerm_resource_group.novikovterraformgroup.name
+    location = azurerm_resource_group.novikovTerraformGroup.location
+    resource_group_name = azurerm_resource_group.novikovTerraformGroup.name
     address_space = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "novikovTerraformnSubNet" {
     name = "novikovTerraformnSubNet"
     virtual_network_name = azurerm_virtual_network.novikovTerraformGroupNetwork.name
-    resource_group_name = azurerm_resource_group.novikovterraformgroup.name
+    resource_group_name = azurerm_resource_group.novikovTerraformGroup.name
     address_prefixes = ["10.0.1.0/24"]
 }
